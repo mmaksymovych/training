7ec78386343a40c437a6a3116cced091e69abae2
mmaksymovych <maksym.maksymovych@seasoned.co>
2021-01-15 18:59:46 +0200

    eslint / prettier setup

--- javascript/generators.js
+++ javascript/generators.js
@@ -1,24 +1,27 @@
+/* simple generator example */
 function* generator() {
-  yield 1;
-  yield 2;
-  yield 3;
+  yield 1
+  yield 2
+  yield 3
 }
 
-const gen1 = generator();
+const gen1 = generator()
 
-console.log(gen1.next()); // {value: 1, done: false}
-console.log(gen1.next()); // {value: 2, done: false}
-console.log(gen1.next()); // {value: undefined, done: true}
+console.log(gen1.next()) // {value: 1, done: false}
+console.log('test')
 
-function* observer() {
-  // (A)
-  while (true) {
-    const input = yield; // (B)
-    console.log(input);
+console.log(gen1.next()) // {value: 2, done: false}
+console.log(gen1.next()) // {value: undefined, done: true}
+
+/* generator that accepts value */
+function* acceptGenerator() {
+  const test = yield 1
+  if (test) {
+    console.log(test)
   }
 }
 
-const obs = observer();
-obs.next();
-obs.next('a');
-obs.next('b');
+const test = acceptGenerator()
+
+test.next('bla')
+test.next()
